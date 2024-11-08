e23ba25fbd4de4d680a12463d6976aaa5dea4bcf
somramnani <som.ramnani@gmail.com>
2024-11-07 20:52:24 -0500

    Add Icon to navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -4,6 +4,10 @@ import { useEffect, useState } from "react";
 import { Navbar as ReactNavbar, Nav } from "react-bootstrap";
 import Link from "next/link";
 import { sansitaSwashed } from "../../app/layout";
+import { LuLogIn } from "react-icons/lu";
+import { GrLogin } from "react-icons/gr";
+import { BiSolidLogIn } from "react-icons/bi";
+import { IoIosLogIn } from "react-icons/io";
 
 const Navbar = () => {
   const [isClient, setIsClient] = useState(false);
@@ -17,13 +21,20 @@ const Navbar = () => {
   }
 
   return (
-    <ReactNavbar bg="light" expand="lg">
+    <ReactNavbar
+      bg="light"
+      expand="lg"
+      style={{
+        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+      }}
+    >
       <ReactNavbar.Brand
         as={Link}
         href="/"
         className={sansitaSwashed.className}
         style={{
           fontSize: "1.8em",
+          marginLeft: "10px",
         }}
       >
         Date Night
@@ -31,7 +42,19 @@ const Navbar = () => {
       <ReactNavbar.Toggle aria-controls="basic-navbar-nav" />
       <ReactNavbar.Collapse id="basic-navbar-nav">
         <Nav className="ms-auto">
-          <Nav.Link as={Link} href="/login">
+          <Nav.Link
+            as={Link}
+            href="/login"
+            style={{
+              marginRight: "5px",
+            }}
+          >
+            <IoIosLogIn
+              style={{
+                fontSize: "20px",
+                marginRight: "5px",
+              }}
+            />
             Login
           </Nav.Link>
           {/* You can add more Nav.Link components here if needed */}
