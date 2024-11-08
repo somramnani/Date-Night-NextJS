20a572d9226ce80c3a0ebe421b04f7d36de501af
somramnani <som.ramnani@gmail.com>
2024-11-07 21:03:58 -0500

    Add header text to homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,20 @@
-import styles from "./page.module.css";
 import Navbar from "../components/navbar/navbar";
+import { sansitaSwashed } from "../app/layout";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      Home Page
+      <h1
+        className={sansitaSwashed.className}
+        style={{
+          fontWeight: "bold",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        Plan your date{" "}
+      </h1>
     </div>
   );
 }
